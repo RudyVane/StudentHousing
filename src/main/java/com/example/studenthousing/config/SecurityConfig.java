f30d2c0a9dc12d91a85b106dfa19f288db3320ac
Rudy Vane <rudyvane1965@gmail.com>
2023-05-16 09:35:26 +0200

    @EnableWebSecurity added

--- src/main/java/com/example/studenthousing/config/SecurityConfig.java
+++ src/main/java/com/example/studenthousing/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean

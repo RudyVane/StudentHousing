23fd191da80c5513edbc5d48857d8fa5cdba2681
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-16 16:07:57 +0200

    added error responses for wrong input

--- src/main/java/com/example/studenthousing/controller/RegisterController.java
+++ src/main/java/com/example/studenthousing/controller/RegisterController.java
@@ -3,13 +3,13 @@ package com.example.studenthousing.controller;
 import com.example.studenthousing.model.User;
 import com.example.studenthousing.repository.UserRepository;
 import com.example.studenthousing.services.UserService;
+import com.example.studenthousing.validation.error.UserAlreadyExistException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.*;
 import java.util.Map;
 
 
@@ -40,26 +40,51 @@ public class RegisterController {
         // Check the request for correct input
         if (user == null) {
             return new ResponseEntity<>(Map.of("error", "You have not given the correct input"),
-                    HttpStatus.BAD_REQUEST);
+                    HttpStatus.UNPROCESSABLE_ENTITY);
         }
 
         // Check if all parameters are correctly given, else show error message
         if (user.getUsername() == null || user.getEmail() == null || user.getPassword() == null) {
             return new ResponseEntity<>(Map.of("error", "You have not given the correct input"),
-                    HttpStatus.BAD_REQUEST);
+                    HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        // Check if the passwords are identical
+        if (!user.getPassword().equals(user.getMatching_password())) {
+            return new ResponseEntity<>(Map.of("error", "The passwords are not identical"),
+                    HttpStatus.UNPROCESSABLE_ENTITY);
         }
 
         // Create new user via UserService
-        User newUser = userService.registerNewUser(
-                user.getUsername(),
-                user.getEmail(),
-                user.getPassword());
+        User newUser = null;
+        try {
+            newUser = userService.registerNewUser(
+                    user.getUsername(),
+                    user.getEmail(),
+                    user.getPassword());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            // Display message to user about username or email already existing
+            if (e instanceof UserAlreadyExistException) {
+                return new ResponseEntity<>(Map.of(
+                        "error", e.getMessage()),
+                        HttpStatus.UNPROCESSABLE_ENTITY);
+            }
+            // Other errors are most probably an invalid email, thus show this message
+            // This does not mean other errors are not possible! Can do with improvement.
+            // Tried to catch other exceptions or instanceof exceptions, but couldn't get it working
+            else {
+                return new ResponseEntity<>(Map.of(
+                        "error", "Invalid email!"),
+                        HttpStatus.UNPROCESSABLE_ENTITY);
+            }
+        }
 
+        // Upon successful registering, show the newly registered user
         return new ResponseEntity<>(Map.of(
                 "username", newUser.getUsername(),
                 "email", newUser.getEmail(),
                 "id", newUser.getUser_id(),
-                "password", newUser.getPassword(),
                 "status", "Account created!"),
                 HttpStatus.OK);
     }

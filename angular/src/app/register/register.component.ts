ff3b22109e498f1ccfefae741f670d3972c00a33
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-22 11:23:45 +0200

    changed input fields to match backend

--- angular/src/app/register/register.component.ts
+++ angular/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -15,26 +16,25 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.formFields = [
-      { label: 'First Name:', id: 'firstName', name: 'firstName', required: true },
-      { label: 'Last Name:', id: 'lastName', name: 'lastName', required: true },
+      { label: 'Username:', id: 'username', name: 'username', required: true },
       { label: 'Email:', id: 'email', name: 'email', required: true },
       { label: 'Password:', id: 'password', name: 'password', required: true },
-      { label: 'Confirm Password:', id: 'confirmPassword', name: 'confirmPassword', required: true }
+      { label: 'Confirm Password:', id: 'matching_password', name: 'matching_password', required: true }
     ];
 
     this.form = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
+      username: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
-      confirmPassword: ['', Validators.required]
+      matching_password: ['', Validators.required]
     });
   }
 
   submitForm() {
     if (this.form.valid) {
       const formData = JSON.stringify(this.form.value);
-      this.http.post('http://localhost:8080/studenthousing/properties\'', formData).subscribe(
+      console.log(formData);
+      this.http.post('http://localhost:8080/register', formData).subscribe(
         (response) => {
           console.log('Form submission successful:', response);
           // Handle any success logic here

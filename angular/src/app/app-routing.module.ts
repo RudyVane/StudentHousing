1f0255dfcf54c8d4dfca1fae26a6edf22a5790e1
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-23 15:18:05 +0200

    added account component

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -5,11 +5,15 @@ import { RegisterComponent } from './register/register.component';
 import { PropertyListComponent } from './property-list/property-list.component';
 import {UserAdvertisementComponent} from "./user-advertisement/user-advertisement.component";
 import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
+import {AccountComponent} from "./account/account.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'form', component: FormComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'account', component: AccountComponent },
   { path: 'property-list', component: PropertyListComponent },
   { path: 'user-advertisement', component: UserAdvertisementComponent }
 ];

711bd91a980bec9b03e599c82768dd6a429b3f51
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-23 17:06:06 +0200

    login component POST logic added

--- angular/src/app/login/login.component.ts
+++ angular/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder} from "@angular/forms";
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  userDetails = this.formBuilder.group({
+    username: '',
+    password: ''
+  });
+
+  constructor(
+    private http: HttpClient,
+    private formBuilder: FormBuilder
+  ) {}
+
+  onSubmit(): void {
+    if (this.userDetails.valid) {
+      console.log(this.userDetails.value);
+      this.http.post<any>('/api/login', this.userDetails.value).subscribe(
+        (response) => {
+          console.log('Form submission successful:', response);
+          // Handle any success logic here
+        },
+        (error) => {
+          console.error('Form submission error:', error.message);
+          // Handle any error logic here
+        }
+      );
+    } else {
+      // Form is invalid, display error messages or take appropriate action
+      console.log('There was another error');
+    }
+  }
 }

b11ebf092a7d11b96c8bcc3ea0f678fad2343246
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-23 15:17:15 +0200

    added account component

---
+++ angular/src/app/account/account.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css']
+})
+export class AccountComponent {
+
+}

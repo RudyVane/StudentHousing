e1d41736f19f0c06dbe7f2576e05c24bec8b4a07
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-30 11:21:25 +0200

    added logic to fetch a user to display on /account

--- angular/src/app/account/account.component.ts
+++ angular/src/app/account/account.component.ts
@@ -1,25 +1,41 @@
 import {Component, OnInit} from '@angular/core';
 import { HttpClient} from "@angular/common/http";
+import { User } from "../models/user";
+import { AlertService } from "../services/alert.service";
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
-export class AccountComponent {
-  accountDetails: any[] = [];
+export class AccountComponent implements OnInit {
+  user: User = {} as User;
+  isLoading: boolean = true;
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private alertService: AlertService
+  ) { }
 
   ngOnInit(): void {
-    this.getAccountDetails();
+    this.getUser();
   }
 
-  getAccountDetails(): void {
-    this.http.get<any[]>('/api/account')
-      .subscribe(data => {
-        this.accountDetails = data;
-      })
+  private setUser(response: User): void {
+    this.user = response;
+    this.isLoading = false;
   }
 
+  getUser(): void {
+    this.http.get<User>('/api/account').subscribe(
+      (response: User) => {
+        this.setUser(response);
+      },
+      (error) => {
+        console.error('Error fetching user:', error);
+        this.alertService.error("Your user was not found");
+        this.isLoading = false;
+      }
+    );
+  }
 }

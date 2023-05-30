4402cba89ae33fbb19f8db957148ef2480d9a3fd
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-30 09:30:39 +0200

    environment updated for apiURL

--- angular/src/app/environments/environment.ts
+++ angular/src/app/environments/environment.ts
@@ -1,3 +1,8 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
   production: false,
+  apiUrl: 'http://localhost:4000'
 };

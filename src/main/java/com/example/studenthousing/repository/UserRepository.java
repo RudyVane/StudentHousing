fd1ecbba8b338dfb08ddee427fd6887a9ad46955
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-05-30 10:28:22 +0200

    changed user_id to id

--- src/main/java/com/example/studenthousing/repository/UserRepository.java
+++ src/main/java/com/example/studenthousing/repository/UserRepository.java
@@ -4,10 +4,12 @@ import com.example.studenthousing.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
     User findByEmail(String email);
     User findByUsername(String username);
+    User findById(int id);
 
     List<User> findByAdActiveTrue();
 

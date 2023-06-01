2b30b0e9a10961f5e13458a02ddba7de47365e6d
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-06-01 10:32:29 +0200

    added alert for successful registering

--- angular/src/app/register/register.component.ts
+++ angular/src/app/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { AlertService } from "../services/alert.service";
+import {User} from "../models/user";
 
 @Component({
   selector: 'app-register',
@@ -11,15 +13,18 @@ export class RegisterComponent implements OnInit {
   form!: FormGroup;
   formFields!: any[]; // Define the form fields array
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private alertService: AlertService,
+    private http: HttpClient) {}
 
 
   ngOnInit() {
     this.formFields = [
-      { label: 'Username:', id: 'username', name: 'username', required: true },
-      { label: 'Email:', id: 'email', name: 'email', required: true },
-      { label: 'Password:', id: 'password', name: 'password', required: true },
-      { label: 'Confirm Password:', id: 'matching_password', name: 'matching_password', required: true }
+      { label: 'Username:', id: 'username', name: 'username', type: 'text', required: true },
+      { label: 'Email:', id: 'email', name: 'email', type: 'text', required: true },
+      { label: 'Password:', id: 'password', name: 'password', type: 'password', required: true },
+      { label: 'Confirm Password:', id: 'matching_password', name: 'matching_password', type: 'password', required: true }
     ];
 
     this.form = this.formBuilder.group({
@@ -32,16 +37,18 @@ export class RegisterComponent implements OnInit {
 
   submitForm() {
     if (this.form.valid) {
-        this.http.post('/api/register', this.form.value).subscribe(
-          (response) => {
-            console.log('Form submission successful:', response);
-            // Handle any success logic here
-          },
-          (error) => {
-            console.error('Form submission error:', error.error);
-            // Handle any error logic here
-          }
-        );
+      this.http.post<User>('/api/register', this.form.value).subscribe(
+        (response) => {
+          console.log('Form submission successful:', response);
+          // Handle any success logic here
+          let msg = response.username + ` you have successfully registered with id ` + response.id;
+          this.alertService.success(msg);
+        },
+        (error) => {
+          console.error('Form submission error:', error.error);
+          // Handle any error logic here
+        }
+      );
     } else {
       // Form is invalid, display error messages or take appropriate action
     }

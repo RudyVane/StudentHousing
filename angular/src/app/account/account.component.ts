1e89c22cd4532ee0522dd3fde18a7ff81961c6d3
StefanFB <68238380+StefanFB@users.noreply.github.com>
2023-06-01 10:30:40 +0200

    updated form, buttons and logic

--- angular/src/app/account/account.component.ts
+++ angular/src/app/account/account.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import { HttpClient} from "@angular/common/http";
 import { User } from "../models/user";
 import { AlertService } from "../services/alert.service";
+import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -14,10 +15,12 @@ export class AccountComponent implements OnInit {
   user: User = {} as User;
   isLoading: boolean = true;
   number!: number;
-  id: number | null = null;
+  id: number = 79;
+  hasAd: boolean = false;
 
   constructor(
     private http: HttpClient,
+    private router: Router,
     private alertService: AlertService
   ) { }
 
@@ -27,12 +30,14 @@ export class AccountComponent implements OnInit {
 
   private setUser(response: User): void {
     this.user = response;
+    this.id = this.user.id;
     this.isLoading = false;
   }
 
   getUser(): void {
     this.alertService.clear();
     if (this.id === null) {
+      this.hasAd = false;
       this.http.get<User>('/api/account').subscribe(
         (response: User) => {
           this.setUser(response);
@@ -44,14 +49,21 @@ export class AccountComponent implements OnInit {
         }
       );
     } else {
-      const url = `/api/account?id=${this.number}`;
+      const url = `/api/account?id=${this.id}`;
       this.http.get<User>(url).subscribe(
         (response) => {
-          console.log(response);
+          // Read response: error when null response, else update our user with the response
           if (response == null) {
             this.alertService.error("This user does not exist")
           } else {
             this.setUser(response);
+
+            // Check if this user has a advertisement, set flag accordingly (it changes the visible buttons)
+            if (this.user.adActive) {
+              this.hasAd = true;
+            } else {
+              this.hasAd = false;
+            };
           }
         },
         (error) => {
@@ -63,10 +75,36 @@ export class AccountComponent implements OnInit {
     }
   }
 
+  showCreateNewAdForm(id: number) {
+    this.router.navigate(['/user-advertisement'], { queryParams: { id: id } });
+  }
+
   submitForm() {
     if (this.number) {
       this.id = this.number;
     }
     this.getUser();
+  }
+
+  showID() {
+    console.log(this.id);
+  }
+
+  deleteAd(id: number) {
+    this.alertService.clear();
+    this.http.delete(`/api/advertisements/${id}`).subscribe(
+      () => {
+        this.alertService.success("Advertisement has been deleted successfully!")
+        console.log('Advertisement deleted successfully');
+      },
+      (error) => {
+        this.alertService.error("Something went wrong, please try again.")
+        console.error('Error deleting advertisement:', error);
+      }
+    );
+  }
+  editAd(id: number) {
+    this.alertService.clear();
+    this.router.navigate(['/user-advertisement'], { queryParams: { id: id } });
   }
 }
